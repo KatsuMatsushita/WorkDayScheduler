701306ca4dcc802fbc7f1c077747a9e998ab7e8c
Hungry Man <katsu.matsushita@gmail.com>
2021-07-30 18:47:26 -0400

    Added a way for the time-blocks to be given the past, present, and future classes. Will now need to check and remove the invalid classes. Converted the time to 24H time without losing the 12H time for the Labels. Still need to implement local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,23 +17,29 @@ function initGenerateTimeblocks(){
         var blockLabel = $("<label>");
         var blockText = $("<textarea>");
         var blockBtn = $("<button>");
-    
+        var amPM = hour.split(" ");
+
+        if (amPM[1] == "PM" && amPM[0] != "12"){
+            amPM[0] = parseInt(amPM[0]) + 12;
+        };
+        
+
         timeBlock.addClass("row time-block");
 
         blockLabel.addClass("hour col");
         blockLabel.attr("for", function(){
-            return "userTask" + hour.split(" ", 1);
+            return "userTask" + amPM[0];
         });
         blockLabel.text(hour);
 
         blockText.addClass("col-8");
         blockText.attr("id", function(){
-            return "userTask" + hour.split(" ", 1);
+            return "userTask" + amPM[0];
         });
         blockText.attr("name", function(){
-            return "userTask" + hour.split(" ", 1);
+            return "userTask" + amPM[0];
         });
-        blockText.attr("data-hour", hour.split(" ", 1));
+        blockText.attr("data-hour", amPM[0]);
         
         blockBtn.addClass("saveBtn");
         blockBtn.text("Save");
@@ -57,27 +63,32 @@ function displayTime(){
     //var checkingTimeBlock = timeBlockEl.children(timeBlockEl.children.attr(hour));
     // change the class of the userTask to past, present, or future depending on the currentTime
     arrHours.forEach(function (hour, index) {
-        // checkHour is the hour and timeBlock to be checked
-        var changeHour = "[data-hour=" + hour.split(" ", 1) + "]";
-        var checkHour = hour.split(" ", 1);
-        //timeBlockEl.find("[data-hour=9]").addClass("past");
+        // amPm is the hour and timeBlock to be checked
+        var amPM = hour.split(" ");
+
+        if (amPM[1] == "PM" && amPM[0] != "12"){
+            amPM[0] = parseInt(amPM[0]) + 12;
+        };
+
+        var changeHour = "[data-hour=" + amPM[0] + "]";
         
-        if ( currentHour == checkHour){
+        //convert the checkHour to 24H time to fit with the data-hour attribute
+        
+        if ( currentHour == amPM[0]){
             timeBlockEl.find(changeHour).addClass("present");
-        } else if (checkHour < currentHour){
+        } else if (amPM[0] < currentHour){
             timeBlockEl.find(changeHour).addClass("past");
         } else {
             timeBlockEl.find(changeHour).addClass("future");
-            console.log(timeBlockEl.find("textarea").filter(changeHour));
         }
     })
 };
 
 function saveTask(event){
-    console.log(event.target);
     var btnClicked = $(event.target);
-    console.log(btnClicked);
-    console.log(btnClicked.parent);
+    //console.log(btnClicked.prev());
+    console.log(btnClicked.prev().val());
+
 }
 
 timeBlockEl.on("click", ".saveBtn", saveTask)
